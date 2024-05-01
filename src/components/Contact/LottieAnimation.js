5306cc28f05acb083ab91478daa0c163a2aa821c
Victor <vjans@students.uni-mainz.de>
2024-05-01 13:36:04 +0200

    improved performance

--- src/components/Contact/LottieAnimation.js
+++ src/components/Contact/LottieAnimation.js
@@ -32,54 +32,11 @@ export function createDemo(canvas, modelName, W, H) {
     canvas.height = 960; // Adjusted from H*6
 
     function updateUI() {
-    	/*
-      $$('#pattern-selector *').forEach(e=>{
-        e.style.opacity = e.id==target ? 1.0 : 0.2;
-      });
-      $$('#model-selector input').forEach(e=>{
-        e.checked = e.id==experiment;
-      });
-      $$('#model-hints span').forEach(e=>{
-        e.style.display = e.id.startsWith(experiment) ? "inline" : "none";
-      });
-      $('#play').style.display = paused? "inline" : "none";
-      $('#pause').style.display = !paused? "inline" : "none";
-      const speed = parseInt($('#speed').value);
-      $('#speedLabel').innerHTML = ['1/60 x', '1/10 x', '1/2 x', '1x', '2x', '4x', '<b>max</b>'][speed+3];
-      $("#rotationLabel").innerText = $('#rotation').value + '°';
-    	*/
+    	
     }
 
     function initUI() {
-      let spriteX = 0;
-      for (let c of '🦎😀💥👁🐠🦋🐞🕸🥨🎄') {
-        const div = document.createElement('div')
-        div.id = c;
-        div.style.backgroundPositionX = spriteX + 'px';
-        div.onclick = ()=>{
-          target = c;
-          updateModel();
-        }
-        spriteX -= 40;
-        //$('#pattern-selector').appendChild(div);
-      }
-      //$('#reset').onclick = demo.reset;
-      /*$('#play-pause').onclick = ()=>{
-        paused = !paused;
-        updateUI();
-      };*/
-      /*
-      $$('#model-selector input').forEach(sel=>{
-        sel.onchange = ()=>{
-          experiment = sel.id;
-          
-        }
-      });*/
       	updateModel();
-      //$('#speed').onchange = updateUI;
-      //$('#speed').oninput = updateUI;
-      //$('#rotation').onchange = updateUI;
-      //$('#rotation').oninput = updateUI;
 
       function canvasToGrid(x, y) {
         const [w, h] = demo.gridSize;
@@ -131,16 +88,6 @@ export function createDemo(canvas, modelName, W, H) {
           move(getMousePos(e));
         }
       }
-      canvas.addEventListener("touchstart", e=>{
-        e.preventDefault();
-        click(getTouchPos(e.changedTouches[0]));
-      });
-      canvas.addEventListener("touchmove", e=>{
-        e.preventDefault();
-        for (const t of e.touches) {
-          move(getTouchPos(t));
-        }
-      });
       updateUI();
     }
 
@@ -161,10 +108,6 @@ export function createDemo(canvas, modelName, W, H) {
     }
     updateModel();
   
-    let lastDrawTime = 0;
-    let stepsPerFrame = 1;
-    let frameCount = 0;
-  
     function render(time) {
 
       if  (!isInViewport(canvas)) {
@@ -173,34 +116,17 @@ export function createDemo(canvas, modelName, W, H) {
       }
   
       if (!paused) {
-        const speed = 1;
-        if (speed <= 0) {  // slow down by skipping steps
-          const skip = [1, 2, 10, 60][-speed];
-          stepsPerFrame = (frameCount % skip) ? 0 : 1;
-          frameCount += 1;
-        } else if (speed > 0) { // speed up by making more steps per frame
-          const interval = time - lastDrawTime;
-          stepsPerFrame += interval<20.0 ? 1 : -1;
-          stepsPerFrame = Math.max(1, stepsPerFrame);
-          stepsPerFrame = Math.min(stepsPerFrame, [1, 2, 4, Infinity][speed])
-        }
-        demo.setAngle(0);
-        for (let i=0; i<stepsPerFrame; ++i) {
-          demo.step();
-        }
+        demo.step();
         demo.getStepCount();
-        //$("#stepCount").innerText = demo.getStepCount();
-        //$("#ips").innerText = demo.fps();
 
           // Chance to randomly paint a point
-        if (Math.random()*Math.random() > 0.9 && modelName=='/victor.json') { // 10% chance to paint a point
+        if (Math.random() > 0.9975 && modelName=='/victor.json') { // 10% chance to paint a point
             const x = Math.floor(Math.random() * W); // Random x-coordinate
             const y = Math.floor(Math.random() * H); // Random y-coordinate
             demo.paint(x, y, 1, 'seed');
         }
 
         }
-      lastDrawTime = time;
 
       twgl.bindFramebufferInfo(gl);
       demo.draw();
@@ -224,7 +150,7 @@ const LottieAnimation = ({ modelName, width, height }) => {
         // Delay the rendering of the canvas by 2 seconds
         const timer = setTimeout(() => {
             setShowCanvas(true);
-        }, 2000); // 2000 milliseconds delay
+        }, 1); // 2000 milliseconds delay
 
         return () => clearTimeout(timer); // Cleanup the timer
     }, []); // Empty dependency array ensures this runs once on mount
@@ -259,7 +185,7 @@ const LottieAnimation = ({ modelName, width, height }) => {
 
     return (
         <div className="flex items-center justify-center w-full h-full">
-            {showCanvas && <canvas ref={canvasRef} className="border-2 border-black dark:border-white"></canvas>}
+            {showCanvas && <canvas ref={canvasRef} className="border-2 border-black dark:border-white" width="960" height="960"></canvas>}
         </div>
     );
 };
